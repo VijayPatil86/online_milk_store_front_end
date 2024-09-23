f67245bdcc3680c1ff168fa7f291adc986614490
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-23 17:17:36 +0530

    added cart related code

--- public/customer/js/milk.js
+++ public/customer/js/milk.js
@@ -41,10 +41,17 @@ function displayMilkBrands(link_getAllAvailableMilkBrands) {
 				}
 				++tableCellCount;
 				html_table = html_table + "<td style='padding-left: 50px; padding-top: 20px; padding-right: 50px; padding-bottom: 13px;'>";
+				html_table = html_table + "<label id='lblMilkBrandNamePackaging_" + milkBrandId + "'";
+				html_table = html_table + " data='" + milkBrandName + "_" + packaging + "_" + price +  "'>";
 				html_table = html_table + milkBrandName + " " + packaging;
+				html_table = html_table + "</label>";
 				html_table = html_table + "<br><label style='padding-left: 45px;'>₹ " + price + "</label>"
 				html_table = html_table + "<br><label style='padding-left: 20px;'>Qty:</label>";
 				html_table = html_table + "<input id='txtMilkBrandId_" + milkBrandId + "' style='width:30px;'>";
+				html_table = html_table + "<br><label id='lblAddToCart' style='padding-left: 15px; color:blue; cursor:pointer;' ";
+				html_table = html_table + "onclick='addToCart(" + milkBrandId + ")'>";
+				html_table = html_table + "Add to Cart";
+				html_table = html_table + "</label>";
 				html_table = html_table + "</td>";
 			}
 			html_table = html_table + "</table>";
@@ -58,4 +65,23 @@ function displayMilkBrands(link_getAllAvailableMilkBrands) {
 	xhttp.onerror = function() {
 		alert("Network Error: Could not send the request. ");
 	}
+}
+
+const cartMapMilkBrandIdQuantity = new Map();
+const cartMapMilkBrandIdMilkBrandProductInfo = new Map();
+
+function addToCart(milkBrandId) {	// milkBrandId 21
+	var quantity = document.getElementById("txtMilkBrandId_" + milkBrandId).value;	// 1
+	cartMapMilkBrandIdQuantity.set(milkBrandId, quantity);
+	var selectedMilkBrandProductInfo = document.getElementById("lblMilkBrandNamePackaging_" + milkBrandId).getAttribute("data");
+	if(cartMapMilkBrandIdMilkBrandProductInfo.has(milkBrandId) == false) {
+		cartMapMilkBrandIdMilkBrandProductInfo.set(milkBrandId, selectedMilkBrandProductInfo);
+		localStorage.setItem("cartMapMilkBrandIdMilkBrandProductInfo", JSON.stringify(Array.from(cartMapMilkBrandIdMilkBrandProductInfo.entries())));
+	}
+	localStorage.setItem("cartMapMilkBrandIdQuantity", JSON.stringify(Array.from(cartMapMilkBrandIdQuantity.entries())));
+	alert("Added to Cart");
+}
+
+function showCartItemsDetails() {
+	var cartItemsDetailsWindow = window.open("..\\html\\cartItemsDetails.html", "", "width=800,height=800");
 }
