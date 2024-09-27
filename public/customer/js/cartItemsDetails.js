ab7208a91b2fca37903dd9caf48abeb91e500590
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-27 15:26:53 +0530

    added selected product availability column wip - check selected products availability in inventory

--- public/customer/js/cartItemsDetails.js
+++ public/customer/js/cartItemsDetails.js
@@ -4,6 +4,7 @@ function loadCartItemsDetails() {
 		var entries = JSON.parse(cartData);
 		var cartMapMilkBrandIdQuantity = new Map(entries);
 
+		checkSelectedProductsAvailability(cartMapMilkBrandIdQuantity);
 		var cartProductInfoData = localStorage.getItem("cartMapMilkBrandIdMilkBrandProductInfo");
 		var entriesSelectedProductInfo = JSON.parse(cartProductInfoData);
 		var cartMapMilkBrandIdMilkBrandProductInfo = new Map(entriesSelectedProductInfo);
@@ -15,6 +16,7 @@ function loadCartItemsDetails() {
 		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Price" + "</th>";
 		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Quantity" + "</th>";
 		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Amount" + "</th>";
+		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Availability" + "</th>";
 		table_SelectedProductInfo += "</tr>";
 		var totalAmount = 0;
 		cartMapMilkBrandIdQuantity.forEach((quantity, milkBrandId) => {
@@ -25,7 +27,9 @@ function loadCartItemsDetails() {
 			table_SelectedProductInfo += "<td style='border: 1px solid black; font-size: large'>" + splitSelectedProductInfo[1] + "</td>";
 			table_SelectedProductInfo += "<td align='center' style='border: 1px solid black; font-size: large'>" + splitSelectedProductInfo[2] + "</td>";
 			table_SelectedProductInfo += "<td align='center' style='border: 1px solid black; font-size: large'>" + quantity + "</td>";
-			table_SelectedProductInfo += "<td align='right' style='border: 1px solid black; font-size: large'>" + (quantity * splitSelectedProductInfo[2]) ;
+			table_SelectedProductInfo += "<td align='right' style='border: 1px solid black; font-size: large'>" + (quantity * splitSelectedProductInfo[2]) + "</td>";
+			table_SelectedProductInfo += "<td align='center' style='border: 1px solid black; font-size: large'>";
+			table_SelectedProductInfo += "<img id='imgAvailability' src='..\\images\\Tick_Mark_Circle-Green.webp' style='height:30px; width:30px;'>";
 			table_SelectedProductInfo += "</td>";
 			table_SelectedProductInfo += "</tr>";
 			totalAmount += quantity * splitSelectedProductInfo[2];
@@ -49,4 +53,15 @@ function closeWindow() {
 	localStorage.removeItem("cartMapMilkBrandIdQuantity");
 	localStorage.removeItem("cartMapMilkBrandIdMilkBrandProductInfo");
 	window.close();
+}
+
+function checkSelectedProductsAvailability(cartMapMilkBrandIdQuantity) {
+	var queryParams = "";
+	cartMapMilkBrandIdQuantity.forEach((quantity, milkBrandId) => {
+		queryParams += milkBrandId + "=" + quantity + "&";
+	});
+	if (queryParams.endsWith('&')) {
+	    queryParams = queryParams.slice(0, -1);
+	}
+	alert(queryParams);	// 21=2&22=1
 }
