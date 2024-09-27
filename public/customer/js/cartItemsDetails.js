54fd526fbd64a4d9e1dfb4e36dae4c65291f25ba
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-27 18:00:15 +0530

    added code to validate quantity of selected products against inventory and show the status

--- public/customer/js/cartItemsDetails.js
+++ public/customer/js/cartItemsDetails.js
@@ -3,8 +3,6 @@ function loadCartItemsDetails() {
 	if (cartData) {
 		var entries = JSON.parse(cartData);
 		var cartMapMilkBrandIdQuantity = new Map(entries);
-
-		checkSelectedProductsAvailability(cartMapMilkBrandIdQuantity);
 		var cartProductInfoData = localStorage.getItem("cartMapMilkBrandIdMilkBrandProductInfo");
 		var entriesSelectedProductInfo = JSON.parse(cartProductInfoData);
 		var cartMapMilkBrandIdMilkBrandProductInfo = new Map(entriesSelectedProductInfo);
@@ -17,9 +15,11 @@ function loadCartItemsDetails() {
 		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Quantity" + "</th>";
 		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Amount" + "</th>";
 		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Availability" + "</th>";
+		table_SelectedProductInfo += "<th style='border: 1px solid black;'>" + "Order Upto" + "</th>";
 		table_SelectedProductInfo += "</tr>";
 		var totalAmount = 0;
 		cartMapMilkBrandIdQuantity.forEach((quantity, milkBrandId) => {
+			var imageAvailability = "..\\images\\Tick_Mark_Circle-Green.webp";
 			table_SelectedProductInfo += "<tr>";
 			var selectedProductInfo = cartMapMilkBrandIdMilkBrandProductInfo.get(milkBrandId);	// Warana_250 ML_20
 			var splitSelectedProductInfo = selectedProductInfo.split("_");
@@ -29,8 +29,8 @@ function loadCartItemsDetails() {
 			table_SelectedProductInfo += "<td align='center' style='border: 1px solid black; font-size: large'>" + quantity + "</td>";
 			table_SelectedProductInfo += "<td align='right' style='border: 1px solid black; font-size: large'>" + (quantity * splitSelectedProductInfo[2]) + "</td>";
 			table_SelectedProductInfo += "<td align='center' style='border: 1px solid black; font-size: large'>";
-			table_SelectedProductInfo += "<img id='imgAvailability' src='..\\images\\Tick_Mark_Circle-Green.webp' style='height:30px; width:30px;'>";
-			table_SelectedProductInfo += "</td>";
+			table_SelectedProductInfo += "<img id='imgAvailability_" + milkBrandId + "' src='" + imageAvailability + "' style='height:30px; width:30px;'>" + "</td>";
+			table_SelectedProductInfo += "<td id='cell_" + milkBrandId + "' style='border: 1px solid black; font-size: large'></td>";
 			table_SelectedProductInfo += "</tr>";
 			totalAmount += quantity * splitSelectedProductInfo[2];
 		});
@@ -44,24 +44,57 @@ function loadCartItemsDetails() {
 		table_SelectedProductInfo += "</tr>";
 		table_SelectedProductInfo += "</table>";
 		document.getElementById("div_Content").innerHTML = table_SelectedProductInfo;
+		checkSelectedProductsAvailability(cartMapMilkBrandIdQuantity)
+			.then(map => {
+				map.forEach((value, key) => {
+					document.getElementById('imgAvailability_'+key).src="..\\images\\Cross_Mark_Circle-Red.jpg";
+					document.getElementById('imgAvailability_'+key).style="height:20px; width:20px;";
+					document.getElementById('cell_'+key).innerHTML = value;
+				});
+			});
 	} else {
 		alert("No items in the cart.");
 	}
 }
 
 function closeWindow() {
-	localStorage.removeItem("cartMapMilkBrandIdQuantity");
-	localStorage.removeItem("cartMapMilkBrandIdMilkBrandProductInfo");
 	window.close();
 }
 
 function checkSelectedProductsAvailability(cartMapMilkBrandIdQuantity) {
-	var queryParams = "";
-	cartMapMilkBrandIdQuantity.forEach((quantity, milkBrandId) => {
-		queryParams += milkBrandId + "=" + quantity + "&";
+	return new Promise((resolve, reject) => {
+		var queryParams = "";
+		cartMapMilkBrandIdQuantity.forEach((quantity, milkBrandId) => {
+			queryParams += milkBrandId + "=" + quantity + "&";
+		});
+		if (queryParams.endsWith('&')) {
+		    queryParams = queryParams.slice(0, -1);
+		}
+		var mapMilkBrandIdQuantityAvailable = new Map();
+		var url_checkSelectedProductsAvailability=
+			"http://localhost:9003/oms/inventory-service/inventory/milk-brand?" + queryParams;	// queryParams 21=2&22=4
+		var xhttp = new XMLHttpRequest();
+			xhttp.open("GET", url_checkSelectedProductsAvailability);
+			xhttp.send();
+			xhttp.onload = function() {
+				if(xhttp.status == 200){
+					var responseProductIdsQuantitiesAvailable = JSON.parse(this.responseText);
+					// 	21_36,22_26	length=2
+					if(responseProductIdsQuantitiesAvailable.length > 0) {
+						var stringProductIdsQuantitiesAvailable = responseProductIdsQuantitiesAvailable.toString();
+						var arrResponseProductIdsQuantitiesAvailable = stringProductIdsQuantitiesAvailable.split(',');
+						arrResponseProductIdsQuantitiesAvailable.forEach(productIdQuantityAvailable => {
+							var arrProductIdQuantityAvailable = productIdQuantityAvailable.split('_');
+							var productId = arrProductIdQuantityAvailable[0];
+							var productQuantityAvailable = arrProductIdQuantityAvailable[1];
+							mapMilkBrandIdQuantityAvailable.set(productId, productQuantityAvailable);
+						});
+					}
+					resolve(mapMilkBrandIdQuantityAvailable);
+				}
+			}
+			xhttp.onerror = function() {
+				alert("Network Error: Could not send the request. ");
+			}
 	});
-	if (queryParams.endsWith('&')) {
-	    queryParams = queryParams.slice(0, -1);
-	}
-	alert(queryParams);	// 21=2&22=1
 }
